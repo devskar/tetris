a2cf404f6c49ac3322c9dd3486b02f90cbbb4cd8
oskar <ooskar@gmx.de>
2021-02-20 16:35:43 +0100

    implementing coordinates in Block

--- objects/block.py
+++ objects/block.py
@@ -1,29 +1,51 @@
-import pygame
+import random
 
-RECT_SIZE = 20
+colors = [(250, 176, 0), (241, 79, 28), (107, 126, 247), (150, 46, 19), (0, 163, 232), (124, 180, 0)]
 
 
 class Block:
-    def __init__(self, form, screen):
-        self.screen = screen
-        self.color = (255, 255, 255)
+    def __init__(self, form):
+
+        self.color = self.__get_random_color()
         self.form = form
-        self.solid = False
+
+        # calculates start position on the field
+        self.start_pos = int(5 - len(self.form[0]) / 2)
+
+        self.coordinates = self.calculate_start_coords()
 
         self.speed = 0
-        self.x, self.y = 100, 10
 
     def drop(self):
         self.speed = 20
 
     def update(self):
-        self.y += self.speed
-
-    def draw(self):
-        pygame.draw.rect(self.screen, self.color, [self.x, self.y, RECT_SIZE, RECT_SIZE])
+        pass
 
     def rotate(self):
         pass
 
     def push(self):
         pass
+
+    # adds x and y amount to all coordinates
+    def change_coords(self, x_amount, y_amount):
+        for coord in self.coordinates:
+            coord[0] += x_amount
+            coord[1] += y_amount
+
+    # returns the start position of the block on the field
+    def calculate_start_coords(self):
+        coords = []
+
+        # loops through the form of the block and adds all coordinates
+        for y, row in enumerate(self.form):
+            for x, column in enumerate(self.form[y]):
+                # checks if a square belongs to the position
+                if column != '0':
+                    coords.append((self.start_pos + x, y))
+        return coords
+
+    # returns a random color
+    def __get_random_color(self):
+        return colors[random.randint(0, len(colors) - 1)]
